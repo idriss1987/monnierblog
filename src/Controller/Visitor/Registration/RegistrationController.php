bd3097726a12e84a450c705705b3d1093f652582
mohamed <adeboss_3@hotmail.fr>
2022-10-26 21:48:52 +0200

    suite de la mise en place du systeme d authentification par mail

--- src/Controller/Visitor/Registration/RegistrationController.php
+++ src/Controller/Visitor/Registration/RegistrationController.php
@@ -5,28 +5,36 @@ namespace App\Controller\Visitor\Registration;
 
 
 use App\Entity\User;
-use App\Form\RegistrationFormType;
 use App\Service\SendEmailService;
+use App\Form\RegistrationFormType;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'visitor.registration.register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, TokenGeneratorInterface $tokenGenerator, SendEmailService $sendEmailService): Response
     {
+        if ($this->getUser())
+         {
+             return $this->redirectToRoute('visitor.welcome.index');
+         }
+         
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-
+           
             // generate the token for email verification
             $tokenGenerated = $tokenGenerator->generateToken();
             $user->setTokenForEmailVerification($tokenGenerated);
@@ -67,14 +75,62 @@ class RegistrationController extends AbstractController
     }
 
     #[Route('/register/waiting-for-email-verification', name: 'visitor.registration.waiting_for_email_verification')]
-    public function emailVerification(): Response
+    public function waitingForEmailVerification(): Response
     {
         return $this->render("pages/visitor/registration/waiting_for_email_verification.html.twig");
     }
 
     #[Route('/register/email-verification/{id}/{token}', name: 'visitor.registration.email_verification')]
-    public function emailVerification(User $user, $token)
+    public function emailverification(User $user, $token, UserRepository $userRepository)
     {
-        // Pause
+        // si l'utilisateur n'existe pas on refuse l'acces
+        if (! $user) 
+        {
+            throw new AccessDeniedException();
+        }
+        
+        // Si l'utilisateur a déjà vérifié son compte, on le redirige vers la page de connection avec le petit message
+        // lui disant qu'il a déjà vérifié son compte
+        if ($user->isIsVerified() ) 
+        {
+            $this->addFlash("warning","votre compte a déja été verfié, veuillez vous connecter");
+            return $this->redirectToRoute("visitor.welcome.index");
+        }
+
+        // Si le token récupéré depuis l'email est vide
+        // Ou que le token qui a été inséré en tant que propriéte de l'utilisateur est vide
+        // Ou que le token récupéré n'est pas la même chose que le token de l'utilisateur
+        // On refuse l'accès!
+        if ( empty($token) || ($user->getTokenForEmailVerification() == null) || ($token !== $user->getTokenForEmailVerification() ) ) 
+        {
+            throw new AccessDeniedException();
+         }
+
+         // Si l'instant t durant lequel, cette vérification du compte est en trai n d'être faite 
+        // est > à la date d'expiration prévue, 
+        // cela veut donc dire que la date d'expiration est déjà passée
+
+         if (new \DateTimeImmutable('now') > $user->getTokenForEmailVerificationExpiresAt())
+         {
+            $deadline = $user->getTokenForEmailVerificationExpiresAt();
+            $userRepository->remove($user,true);
+            throw new CustomUserMessageAccountStatusException ("votre délai de validation du compte a expiré le :$deadline. Veuillez vous inscrire à nouveau.");
+         }
+
+         //on verifie le compte
+         $user->setIsVerified(true);
+
+         $user->setTokenForEmailVerification('');
+
+        //  on génére la date a laquelle le compte a été verifié
+        $user->setVerifiedAt(new \DateTimeImmutable('now'));
+
+        // On demande au userRepository de proceder a la modification
+        $userRepository->save($user,true);
+
+        $this->addFlash("success","Votre compte a été vérifié.Veuillez vous connecter!");
+
+        // on redirige l 'utilisateur vers la page de connexion
+        return $this->redirectToRoute("visitor.welcome.index");
     }
 }
